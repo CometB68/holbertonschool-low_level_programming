b8eecf5b7e9a3e716163396ae25a4d5344f9a6e3
CometB68 <cometB68@gmail.com>
2021-10-01 17:01:36 -0700

    ejercicio 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Last digit of %i is ", n);
 	last > 5 ? printf("%i and is greater than 5\n", last)
-		: n == 0 ? printf("%i and is 0\n", last)
+		: last == 0 ? printf("%i and is 0\n", last)
 		: printf("%i and is less than 6 and not 0\n", last);
 	return (0);
 }
